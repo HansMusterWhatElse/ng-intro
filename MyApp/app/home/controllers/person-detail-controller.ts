2b0fdf0917c7e624146a99f9b66411528ddf3d16
Felix Egli <felix.egli@phonak.com>
2015-06-28 13:37:51 +0200

    Modified - "Popup"

--- MyApp/app/home/controllers/person-detail-controller.ts
+++ MyApp/app/home/controllers/person-detail-controller.ts
@@ -11,18 +11,33 @@ module Home.PersonDetailCtrl {
     ctrlName: string;
     personId: string;
     person: Home.Data.IPerson;
+    onRouteChangeOff : any;
+    modified: boolean = false;
 
-    public static $inject = ['$routeParams', '$http', 'Repository'];
+    public static $inject = ['$routeParams', '$http', 'Repository', '$rootScope', '$window'];
 
     // dependencies are injected via AngularJS $injector
-    constructor($routeParams : IPersonalDetailRouteParams, private $http : ng.IHttpService, private repository : Home.Services.IRepository) {
+    constructor($routeParams : IPersonalDetailRouteParams, private $http : ng.IHttpService, private repository : Home.Services.IRepository, private $rootScope :ng.IRootScopeService, private $window : ng.IWindowService) {
       var vm = this;
       vm.ctrlName = 'PersonDetailCtrl';
 
       this.person = repository.getPerson(parseInt($routeParams.personId));
+
+      this.onRouteChangeOff = this.$rootScope.$on('$locationChangeStart', (event, newUrl) => {
+        this.onRouteChangeOff();
+
+        //Navigate to newUrl if the form isn't dirty
+        //if (!$scope.editForm.$dirty) return;
+        if (!this.modified) {
+          return;
+        } else {
+          this.$window.alert('Achtung Daten Modifiziert!');
+        }
+
+        event.preventDefault();
+        return;
+      });
     }
-
-
   }
 
   angular
